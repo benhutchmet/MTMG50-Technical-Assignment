057135b0c5e10bd01b9a8e94665e986700aa1e26
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-03-01 21:46:29 +0000

    updated the plotting time series function

--- plotting_time_series.py
+++ plotting_time_series.py
@@ -3,16 +3,15 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from netCDF4 import Dataset
 import pandas as pd
+from netCDF4 import Dataset
 import datetime
 from datetime import date
 from datetime import timedelta
-# %%
+
 
 # import the data
-f = Dataset(r"C:\Users\Ben Hutchins\OneDrive - University of "
-            r"Reading\Documents\GitHub\MTMG50-Technical-Assignment\japan_ERA5land_20170601-20190801_tokuyama.nc", 'r')
+f = Dataset(r'C:\Users\Ben Hutchins\OneDrive - University of Reading\Documents\GitHub\MTMG50-Technical-Assignment\japan_ERA5land_20170601-20190801_tokuyama.nc', 'r')
 # import the runoff data
 runoff = f.variables['ro'][:,0,0]
 # import the time data
@@ -25,9 +24,8 @@ newcal = [start + datetime.timedelta(hours = i) for i in time]
 # check the time data
 
 
-print(newcal)
+#print(newcal)
 
-# %%
 
 # create a pandas dataframe with the time data and runoff data
 df = pd.DataFrame({'time': newcal, 'runoff': runoff})
@@ -36,10 +34,34 @@ df.rename(columns={'runoff': 'runoff (m)'}, inplace=True)
 # check the dataframe
 df
 
-# %%
 
 # plot the data
-plt.plot(df['time'], df['runoff (m)'])
-plt.show()
+#plt.plot(df['time'], df['runoff (m)'])
+#plt.show()
+
+
+# create a function which will plot the data for any given start and end date
+def plot_data(start, end):
+    """Function which plots a time series of runoff at the tokuyama station for a given start and end date.
+
+    Args:
+        start (str): The start date in the format 'YYYY-MM-DD'.
+        end (str): The end date in the format 'YYYY-MM-DD'.
+
+    Returns:
+        None.
+    """
+
+    condition = df['time'].between(start, end)
+    timeframe_code = df.loc[condition]
+    plt.plot(timeframe_code['time'], timeframe_code['runoff (m)'])
+    plt.show()
+
+
+# test the function with the 2017 data
+plot_data('2017-06-01', '2017-09-30')
+
+# test the function with the 2018 data
+plot_data('2018-06-01', '2018-09-30')
 
 # %%
